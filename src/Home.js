61e2fafce1ad800b7ad125f708cb27ed56d43f2f
Joseph Konop <joseph@netengine.com.au>
2018-03-07 22:41:58 +1000

    routes and first react component

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <h1>This the first react component for Predicta</h1>
+    );
+  }
+}
+
+export default Home;
