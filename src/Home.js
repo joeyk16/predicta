2c1943d5e4fae2d8ad118deb3ed49edbf541e3c6
Joseph Konop <joseph@netengine.com.au>
2018-03-09 08:01:56 +1000

    user can upload image

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import imagesService from './services/imagesService.js';
+const ReactS3Uploader = require('react-s3-uploader');
 
 class Home extends Component {
+  constructor() {
+    super()
+
+    this.imagesService = new imagesService();
+  }
+
+  getSignedUrl = (file, callback) => {
+    this.imagesService.upload(file, callback)
+  }
+
   render() {
     return (
-      <h1>This the first react component for Predicta</h1>
+      <div className="container">
+        <h1>Predicta</h1>
+        <section>
+          <div className="dropzone">
+            <ReactS3Uploader
+              getSignedUrl={this.getSignedUrl.bind(this)}
+              accept="image/*"
+              uploadRequestHeaders={{
+                'x-amz-acl': 'public-read'
+              }}
+              contentDisposition="auto"
+            />
+          </div>
+        </section>
+      </div>
     );
   }
 }
