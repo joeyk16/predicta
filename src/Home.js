78953d9166c92e9118f59be8e8ba9fcc29ba0bc2
joeyk16 <joeyk16@users.noreply.github.com>
2018-03-11 14:28:34 +1000

    Upload image (#1)
    * user can upload image
    
    * image service
    
    * now receiving list of image urls
    
    * images now showing
    
    * environment varibles
    
    * refractoring image upload

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
+import imagesService from './services/imagesService.js';
+const ReactS3Uploader = require('react-s3-uploader');
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      imageUrls: [],
+    };
+  }
+
+  componentWillMount() {
+    this.imagesService = new imagesService();
+    this.imagesList()
+  }
+
+  getSignedUrl = (file, callback) => {
+    this.imagesService.create(file, callback)
+    // TODO: Remove reload and update state
+    window.location.reload();
+  }
+
+  imagesList() {
+    this.imagesService.index(this.imageUrls, this.setImageUrls)
+  }
+
+  setImageUrls = (imageUrls) =>  {
+    this.setState({
+      imageUrls: imageUrls,
+    })
+  }
+
+  imageUrls(s3Images) {
+    return s3Images.Contents.map((image) =>
+      `https://s3.amazonaws.com/predicta-app/${image.Key}`
+    )
+  }
+
   render() {
+    const imageUrls = this.state.imageUrls
+
     return (
-      <h1>This the first react component for Predicta</h1>
+      <div className="container">
+        <h1>Predicta</h1>
+        <section>
+          <div className="dropzone">
+            <ReactS3Uploader
+              getSignedUrl={this.getSignedUrl.bind(this)}
+              accept="image/*"
+              uploadRequestHeaders={{
+                'x-amz-acl': 'public-read'
+              }}
+              contentDisposition="auto"
+            />
+          </div>
+        </section>
+        <section>
+        <div className="row">
+          <div className="col-sm">
+            { imageUrls.map((url) =>
+                <a href={url} className="thumbnail">
+                  <img src={url} alt="..." width="100"></img>
+                </a>
+            )}
+          </div>
+          </div>
+        </section>
+      </div>
     );
   }
 }
---
+++ src/services/imagesService.js
@@ -0,0 +1,42 @@
+import AWS from 'aws-sdk';
+import { awsAccessKey, awsSecretKey } from '../config';
+AWS.config.region = 'us-east-1';
+AWS.config.accessKeyId = awsAccessKey;
+AWS.config.secretAccessKey = awsSecretKey;
+
+const s3Client = new AWS.S3();
+const bucketName = "predicta-app";
+
+export default class imagesService {
+  index(imageUrls, setImageList) {
+    var params = {
+      Bucket: bucketName,
+      MaxKeys: 1000
+    };
+
+    return s3Client.listObjects(params, function(err, s3Images) {
+      if (err) console.log(err, err.stack);
+      else setImageList(imageUrls(s3Images));
+    })
+  }
+
+  create(file, callback) {
+    var params = {
+      Bucket: bucketName,
+      Key: file.name,
+      Expires: 60,
+      ContentType: file.type,
+      ACL: 'public-read',
+    };
+
+    s3Client.getSignedUrl(
+      'putObject',
+      params,
+      (error, signedUrl) => {
+        callback({
+          signedUrl,
+        })
+      }
+    )
+  }
+}
