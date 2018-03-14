e72d24987e3ed260547ddcaec240e5724120ab16
Joseph Konop <joseph@netengine.com.au>
2018-03-14 22:28:07 +1000

    better request

--- src/services/clarifaiApi.js
+++ src/services/clarifaiApi.js
@@ -9,7 +9,7 @@ export default class clarifaiApi {
     return fetch('https://api.clarifai.com/v2/models/Taddar%20Vision/output_info', {
       method: "GET",
       headers: new Headers({
-        'Authorization': 'Key e0949a0683a5466b961cc00b936de86d',
+        'Authorization': `Key ${clarifaiKey}`,
         'Content-Type': 'application/json'
       }),
     })
